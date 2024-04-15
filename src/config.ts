e21de5749e21025f24788f3cbe31f2fa5dd42615
vraspar <v2parikh@uwaterloo.ca>
2024-04-14 23:25:37 -0400

    Update avatar, profile name, and links

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
+  title: 'Vraspar Blogs',
+  subtitle: '',
   lang: 'en',
   themeHue: 250,
   banner: {
@@ -24,37 +24,42 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/vraspar',
       external: true,
     },
   ],
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  avatar: 'assets/images/my-avatar.jpg',
+  name: 'vraspar',
+  bio: 'Just your normal SWE',
   links: [
     {
       name: 'Twitter',
-      icon: 'fa6-brands:twitter',
-      url: 'https://twitter.com',
+      icon: 'fa6-brands:x-twitter',
+      url: 'https://twitter.com/vraspar',
     },
     {
       name: 'Steam',
       icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
+      url: 'https://steamcommunity.com/id/vraspar',
     },
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/vraspar',
     },
+    {
+      name: 'goodreads',
+      icon: 'fa6-brands:goodreads-g',
+      url: 'https://www.goodreads.com/user/show/157449422-vrajanv',
+    }
   ],
 }
 
 export const licenseConfig: LicenseConfig = {
-  enable: true,
+  enable: false,
   name: 'CC BY-NC-SA 4.0',
   url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
 }
--- src/content/spec/about.md
+++ src/content/spec/about.md
@@ -1,7 +1,17 @@
 # About
-This is the demo site for [Fuwari](https://github.com/saicaca/fuwari).
+
+Hi! 
+
+
+I'm a software engineer who loves to create things. I'm passionate about building software that is easy to use, maintainable, and scalable. I'm also interested in learning new technologies and tools to improve my skills. 
+
+I created this blog to share my thoughts, experiences, and projects with the world. I hope you find something useful or interesting here. Feel free to reach out to me if you have any questions or feedback.
+
+Thanks for visiting!
+
 
 > ### Sources of images used in this site
-> - [Unsplash](https://unsplash.com/)
+> - [Profile Picture](https://www.instagram.com/xzrnl/p/CuPYCKEI-wp/?img_index=2)
+<!-- > - [Unsplash](https://unsplash.com/)
 > - [星と少女](https://www.pixiv.net/artworks/108916539) by [Stella](https://www.pixiv.net/users/93273965)
-> - [Rabbit - v1.4 Showcase](https://civitai.com/posts/586908) by [Rabbit_YourMajesty](https://civitai.com/user/Rabbit_YourMajesty)
+> - [Rabbit - v1.4 Showcase](https://civitai.com/posts/586908) by [Rabbit_YourMajesty](https://civitai.com/user/Rabbit_YourMajesty) -->
